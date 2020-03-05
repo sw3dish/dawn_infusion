bdb48db4c6f2c6465c97a048472140bff17b60a8
Colin Burr <colin.a.burr@gmail.com>
2020-03-04 20:35:04 -0500

    Add to newsletter back-end

--- app/app/controllers/newsletters_controller.rb
+++ app/app/controllers/newsletters_controller.rb
@@ -9,5 +9,27 @@ class NewslettersController < ApplicationController
 
   def new
     @newsletter = Newsletter.new
+    @stories = Story.all
+  end
+
+  def create
+    @newsletter = Newsletter.new(newsletter_params)
+
+    if @newsletter.save
+      @newsletter.stories << Story.where(story_params)
+      redirect_to @newsletter
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def newsletter_params
+    params.require(:newsletter).permit(:title, :publish_date)
+  end
+
+  def story_params
+    params.require(:newsletter).permit(story_id: [])
   end
 end
--- app/app/models/newsletter.rb
+++ app/app/models/newsletter.rb
@@ -1,4 +1,6 @@
 class Newsletter < ApplicationRecord
+  validates :title, presence: true
+
   has_many :newsletter_items
   has_many :stories, :through => :newsletter_items
 end
